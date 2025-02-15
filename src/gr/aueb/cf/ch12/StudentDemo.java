15d7f46d95ac5d8349516e9d85dc590031a0fa90
Ang. Stamatis <angelos_stamatis@outlook.com>
2025-02-15 20:51:38 +0200

    Getters - Setters intro

--- src/gr/aueb/cf/ch12/StudentDemo.java
+++ src/gr/aueb/cf/ch12/StudentDemo.java
@@ -3,6 +3,14 @@ package gr.aueb.cf.ch12;
 public class StudentDemo {
 
     public static void main(String[] args) {
-        Student alice = new Student();
+        Student student = new Student();
+
+        student.setId(1);               //student.id = 1;
+        student.setFirstname("Alice");  //student.firstname = "Alice";
+        student.setLastname("W.");      //student.lastname = "W.";
+
+        System.out.println("Id: " + student.getId());
+        System.out.println("Firstname: " + student.getFirstname());
+        System.out.println("Lastname: " + student.getLastname());
     }
 }
